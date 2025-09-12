ebdfbd2656d73ec52301b97ae9243796f202046f
PlanT50 <131733327+PlanT50@users.noreply.github.com>
2025-09-12 11:05:18 -0300

    Update ListaDeProcessos.java

--- src/ListaDeProcessos.java
+++ src/ListaDeProcessos.java
@@ -1,1 +1,14 @@
+public class ListaDeProcessos {
+    private Node inicio;
+    private Node fim;
 
+    private class Node {
+        Node proximo;
+        Processo processo;
+
+        Node(Processo processo) {
+            this.processo = processo;
+            this.proximo = new.node;
+        }
+    }
+}
